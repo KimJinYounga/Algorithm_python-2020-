7e362d47f847bed26506490f48a183a4f7c6531a
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-01-14 23:34:12 +0900

    hash-전화번호목록 수정

--- programmers/hash/전화번호목록.py
+++ programmers/hash/전화번호목록.py
@@ -1,17 +1,16 @@
-phone_book=["119", "97674223", "1195524421"]
+
 def solution(phone_book):
     # 짧은 길이 순으로 정렬
     phone_book=sorted(phone_book, key=len)
+
     for i,v in enumerate(phone_book):
         for j in range(i+1, len(phone_book)):
             if v == phone_book[j][:len(v)]:
                 return False
         return True
-print(solution(phone_book))
 
 """
 다른 사람 풀이
-
 def solution(phone_book): 
     for i in range(len(phone_book)): 
         pivot = phone_book[i] 
@@ -20,5 +19,4 @@ def solution(phone_book):
             if pivot[:strlen] == phone_book[j][:strlen]: 
                 return False 
     return True
-
 """
