7f6c225fb5503373f426df1b06d9ae2c9f6c8253
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-01-21 17:03:25 +0900

    stack-queue 주식가격 채점 성공

--- programmers/stack-queue/주식가격.py
+++ programmers/stack-queue/주식가격.py
@@ -2,11 +2,12 @@ from collections import deque
 
 prices = [1,2,3,2,3]
 def solution(prices):
+    prices = deque(prices)
     answer = []
     long = len(prices)
     for i in range(long):
         second = 0
-        target = prices.pop(0)
+        target = prices.popleft()
         for j in prices:
             second += 1
             if target > j:
