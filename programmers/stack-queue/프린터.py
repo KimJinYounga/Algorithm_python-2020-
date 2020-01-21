7b5c89e80c96338d3ad8bead98323ab17601d982
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-01-21 17:36:13 +0900

    stack-queue 프린터 채점 성공

--- programmers/stack-queue/프린터.py
+++ programmers/stack-queue/프린터.py
@@ -1,30 +1,32 @@
-priorities=[2, 1, 3, 2]
+from collections import deque
+priorities=[3,3,4,2]
 location=2
-
 
 def solution(priorities, location):
     answer = 0
     # 1. [우선순위, 인덱스]를 저장할 2차원 배열 printer 선언
     printer = [[v,i] for i,v in enumerate(priorities)]
-    # 2. printer 배열에서 우선순위에 따라 재배열
-    running=True
-    while(running):
-        if len(list(set(priorities))) ==1:
-            running=False
+    printer = deque(printer)
 
+    # 2. printer 배열에서 우선순위에 따라 printed배열로 넘기기
+    printed = []
+
+    while(len(printer)!=0):
         current_doc, index = printer[0]
+
+        # 배열에서 가장 큰 원소일 경우, printer.pop(0) -> printed배열
+        if current_doc == max(printer)[0]:
+            printed.append(printer.popleft())
+
         for i in printer:
             if current_doc < i[0]:
-                printer.append(printer.pop(0))
+                printer.append(printer.popleft())
                 break
 
-        if index == max(printer)[1]:
-            running = False
-
     # 3. location에 해당하는 '인덱스+1' return
-    for i in printer:
+    for i in printed:
         if i[1] == location:
-            answer = printer.index(i)+1
+            answer = printed.index(i)+1
     return answer
 
 print(solution(priorities, location))
