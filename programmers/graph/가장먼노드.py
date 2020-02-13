9974787925ed13500b06d62c759cc58e67d7b88c
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-02-14 03:27:12 +0900

    가장먼노드 문제풀이

---
+++ programmers/graph/가장먼노드.py
@@ -0,0 +1,29 @@
+n = 6
+edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+
+
+def solution(n, edge):
+    graph = [[] for _ in range(n + 1)]
+    distances = [0 for _ in range(n)]
+    is_visit = [False for _ in range(n)]
+    queue = [0]
+    is_visit[0] = True
+
+    for (a, b) in edge:
+        graph[a - 1].append(b - 1)
+        graph[b - 1].append(a - 1)
+
+    while queue:
+        i = queue.pop(0)
+        for j in graph[i]:
+            if is_visit[j] == False:
+                is_visit[j] = True
+                queue.append(j)
+                distances[j] = distances[i] + 1
+
+    distances.sort(reverse=True)
+    answer = distances.count(distances[0])
+
+    return answer
+
+print(solution(n, edge))
